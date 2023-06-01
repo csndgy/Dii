5ec9bce213178054655658ed79936627ab31e03d
csndgy <73069087+csndgy@users.noreply.github.com>
2023-06-01 10:10:10 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ConsoleApp1
+{
+    public class Program
+    {
+        List<Nobel> adatok = new List<Nobel>();
+        static void Main(string[] args)
+        {
+            File.ReadAllLines("nobel.csv").Skip(1).ToList().ForEach(x => new Nobel(adatok.Add(x));
+        }
+    }
+}
